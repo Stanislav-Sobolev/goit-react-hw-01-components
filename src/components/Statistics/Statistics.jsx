7d6e30434d02e89a4dcc80e07a4be304fd9371b6
Stanislav Sobolev <90769547+Stanislav-Sobolev@users.noreply.github.com>
2022-06-24 19:59:34 +0300

    qw

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,5 +1,4 @@
 import PropTypes from 'prop-types';
-import { Children, Fragment } from 'react';
 
 import {
   Container,
@@ -11,36 +10,36 @@ import {
   Percentage,
 } from './Statistics.styled';
 
-export const Statistics = ({ stats }) => {
-  return (
-    <Fragment>
-      {stats.map(stat => {
-        return <Item key={stat.id} />;
-      })}
-    </Fragment>
-  );
-};
-
-// export const Statistics = ({ title, stats }) => {
+// export const Statistics = ({ stats }) => {
 //   return (
-//     <Centralize>
-//       <Container>
-//         {title ? <Title>{title}</Title> : false}
-
-//         <StatList>
-//           {stats.map((item, index) => {
-//             return (
-//               <Item key={item.id} index={index}>
-//                 <Label>{item.label}</Label>
-//                 <Percentage>{item.percentage}</Percentage>
-//               </Item>
-//             );
-//           })}
-//         </StatList>
-//       </Container>
-//     </Centralize>
+//     <Fragment>
+//       {stats.map(stat => {
+//         return <Item key={stat.id} />;
+//       })}
+//     </Fragment>
 //   );
 // };
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <Centralize>
+      <Container>
+        {title ? <Title>{title}</Title> : false}
+
+        <StatList>
+          {stats.map((item, index) => {
+            return (
+              <Item key={item.id} index={index}>
+                <Label>{item.label}</Label>
+                <Percentage>{item.percentage}</Percentage>
+              </Item>
+            );
+          })}
+        </StatList>
+      </Container>
+    </Centralize>
+  );
+};
 
 Statistics.propTypes = {
   title: PropTypes.string,
