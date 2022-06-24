7dc91ee978404303b11879e55d0f0386d705e2bf
Stanislav Sobolev <90769547+Stanislav-Sobolev@users.noreply.github.com>
2022-06-24 19:49:39 +0300

    qwery

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,31 @@
+import styles from './FriendList.module.css';
+
+const FriendItem = ({ avatar, name, isOnline }) => {
+  return (
+    <li className={styles.item}>
+      <span className={isOnline ? styles.statusOn : styles.statusOff}></span>
+      <img
+        className={styles.avatar}
+        src={avatar}
+        alt="User avatar"
+        width="48"
+      />
+      <p className={styles.name}>{name}</p>
+    </li>
+  );
+};
+
+export const FriendList = ({ friends }) => (
+  <ul className={styles.friendList}>
+    {friends.map(friend => {
+      return (
+        <FriendItem
+          key={friend.id}
+          avatar={friend.avatar}
+          name={friend.name}
+          isOnline={friend.isOnline}
+        />
+      );
+    })}
+  </ul>
+);
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
-import { Children } from 'react';
-import statistics from '../Statistics/data.json';
+import { Children, Fragment } from 'react';
+
 import {
   Container,
   Centralize,
@@ -11,23 +11,43 @@ import {
   Percentage,
 } from './Statistics.styled';
 
-export const Statistics = ({ title, stats }) => {
+export const Statistics = ({ stats }) => {
   return (
-    <Centralize>
-      <Container>
-        {title ? <Title>Upload stats</Title> : false}
-
-        <StatList>
-          {statistics.map((item, index) => {
-            return (
-              <Item key={item.id} index={index}>
-                <Label>{item.label}</Label>
-                <Percentage>{item.percentage}</Percentage>
-              </Item>
-            );
-          })}
-        </StatList>
-      </Container>
-    </Centralize>
+    <Fragment>
+      {stats.map(stat => {
+        return <Item key={stat.id} />;
+      })}
+    </Fragment>
   );
+};
+
+// export const Statistics = ({ title, stats }) => {
+//   return (
+//     <Centralize>
+//       <Container>
+//         {title ? <Title>{title}</Title> : false}
+
+//         <StatList>
+//           {stats.map((item, index) => {
+//             return (
+//               <Item key={item.id} index={index}>
+//                 <Label>{item.label}</Label>
+//                 <Percentage>{item.percentage}</Percentage>
+//               </Item>
+//             );
+//           })}
+//         </StatList>
+//       </Container>
+//     </Centralize>
+//   );
+// };
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.array,
+};
+
+Item.propTypes = {
+  key: PropTypes.number,
+  index: PropTypes.number,
 };
