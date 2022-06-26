a4f5bf2f456508afef1944824f19a3fb7bcfb7e2
Stanislav Sobolev <90769547+Stanislav-Sobolev@users.noreply.github.com>
2022-06-26 14:15:37 +0300

    qwe

--- src/components/Transactions/TransactionHistory.jsx
+++ src/components/Transactions/TransactionHistory.jsx
@@ -5,8 +5,6 @@ import {
   TheadStyled,
 } from './TransactionHistory.styled';
 
-import { Component } from 'react';
-
 const Tr = ({ id, type, amount, currency, index }) => (
   <TrStyled index={index}>
     <TdStyled>{type}</TdStyled>
